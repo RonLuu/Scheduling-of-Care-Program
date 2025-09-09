60082eacdb05c359909eb0d97c23c378d2ad9284
Duc Tran <quytrongduct@student.unimelb.edu.au>
2025-09-09 21:16:05 +1000

    allow adding organization on the UI

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ import path from "path";
 import { fileURLToPath } from "url";
 import passport from "passport";
 
-import { connectDB } from "./models/index.js"; 
+import { connectDB } from "./models/index.js";
 import routes from "./routes/index.js";
-import "./middleware/passport.js"; 
+import "./middleware/passport.js";
 
-dotenv.config(); 
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,25 +19,21 @@ app.use(passport.initialize());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
-
 // Serve static files from the React app
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use("/uploads", express.static(path.join(__dirname, "public", "uploads")));
 
-
 app.get(/^\/(?!api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
-app.use('/api', routes);
+app.use("/api", routes);
 
 const start = async () => {
   try {
-    await connectDB();   
+    await connectDB();
     const server = app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
@@ -60,4 +56,3 @@ const start = async () => {
 };
 
 start();
-
