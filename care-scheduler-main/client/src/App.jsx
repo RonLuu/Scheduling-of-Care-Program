4d7ddecaaf5690532e46e0b1856ceabbb6830315
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-09-13 06:53:58 +1000

    Add a link to register.jsx

--- care-scheduler-main/client/src/App.jsx
+++ care-scheduler-main/client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Welcome from '../pages/welcome'
+import Register from '../pages/register'
 // The central to access other pages
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Welcome/>}/>
+      <Route path='/api/register' element={<Register/>}/>
     </Routes>
   )
 }
