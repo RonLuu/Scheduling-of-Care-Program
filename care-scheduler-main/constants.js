0a43928c955d1de981513bb1fee04ba77dbcc787
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-09-13 16:46:46 +1000

    Add an extra URL constant

--- care-scheduler-main/constants.js
+++ care-scheduler-main/constants.js
@@ -1,4 +1,5 @@
 const client_port = 3000
+const client_URL_users = "http://localhost:5000/api/users"
 const server_port = 5000
 const server_URL = "http://localhost:${server_port}/"
-export {client_port, server_port}
+export {client_port, server_port, client_URL_users}
