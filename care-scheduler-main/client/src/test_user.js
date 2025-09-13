a59eb2df7476b3ead53228382aec1b6084741a00
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-09-13 16:47:35 +1000

    Add test users

---
+++ care-scheduler-main/client/src/test_user.js
@@ -0,0 +1,42 @@
+[
+    {
+        "name": "User1_7258",
+        "email": "user1_7258@test.com",
+        "password": "password123",
+        "organizationId": "68bd1e000d0540c92a8f7258",
+        "role": "PoA",
+        "isActive": true
+    },
+    {
+        "name": "User2_7258",
+        "email": "user2_7258@test.com",
+        "password": "password123",
+        "organizationId": "68bd1e000d0540c92a8f7258",
+        "role": "Family",
+        "isActive": true
+    },
+    {
+        "name": "User1_7259",
+        "email": "user1_7259@test.com",
+        "password": "password123",
+        "organizationId": "68bd1e000d0540c92a8f7259",
+        "role": "PoA",
+        "isActive": true
+    },
+    {
+        "name": "User2_7259",
+        "email": "user2_7259@test.com",
+        "password": "password123",
+        "organizationId": "68bd1e000d0540c92a8f7259",
+        "role": "Family",
+        "isActive": true
+    },
+    {
+        "name": "User1_725a",
+        "email": "user1_725a@test.com",
+        "password": "password123",
+        "organizationId": "68bd1e000d0540c92a8f725a",
+        "role": "PoA",
+        "isActive": true
+    }
+]
