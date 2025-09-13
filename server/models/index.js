eda50f35477edd2511eacbfec0ced2b30d5c4e1f
Duc Tran <quytrongduct@student.unimelb.edu.au>
2025-09-14 00:16:56 +1000

    changed to online database (atlas) with modified README.md

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 const { MONGODB_URI } = process.env;
 
@@ -7,7 +8,6 @@ export const connectDB = async () => {
   if (!MONGODB_URI) {
     throw new Error("MONGODB_URI is not set in .env");
   }
-  //.set("debug", true);
   await mongoose.connect(MONGODB_URI);
   console.log("MongoDB connected successfully");
 };
