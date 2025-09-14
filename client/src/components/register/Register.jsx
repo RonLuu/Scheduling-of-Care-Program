c99fb18e12699e5e9516d667922deac814e4bc02
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-09-14 16:51:02 +1000

    Change the Register to only select role

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,14 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import RegisterOrganization from "./RegisterOrganization.jsx";
-import RegisterUser from "./RegisterUser.jsx";
+import RegisterFamily from "./RegisterFamily.jsx";
+// Register: A wrapper for different kind of registry
+// depending on the role the user selected
+const Register = ({onAuthed}) => {
+  const [role, setRole] = useState();
 
-function Register({ onAuthed }) {
+  const roleInputs = {
+    familymember: <RegisterFamily role="Family" />,
+    // caretaker: <RegisterCareTaker />,
+    // admin: <RegisterAdmin />
+  }
+
   return (
-    <div>
-      <RegisterUser onAuthed={onAuthed} />
-      <RegisterOrganization />
+    <div className="wrapper">
+      <div className="card">
+        <h2>Register</h2>
+        <label htmlFor="role">
+          Choose your role:
+        </label>
+        <select className="choose" id="role" value={role} onChange={(e) => setRole(e.target.value)}>
+          <option value="">-- Select a role --</option>
+          <option value="familymember">Family Member/POA</option>
+          <option value="admin">Admin</option>
+          <option value="caretaker">Caretaker</option>
+        </select>
+        {role && roleInputs[role]}
+      </div>
     </div>
   );
 }
 
-export default Register;
+export default Register
+
+// function Register({ onAuthed }) {
+//   return (
+//     <div>
+//       <RegisterUser onAuthed={onAuthed} />
+//       <RegisterOrganization />
+//     </div>
+//   );
+// }
+
+// export default Register;
