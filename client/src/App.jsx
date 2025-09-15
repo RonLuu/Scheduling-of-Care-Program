cb7b02e164da3aba707581cef6393106c91f5fa8
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-09-16 08:28:24 +1000

    Add the register organization page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import {Routes, Route} from 'react-router-dom'
 import Welcome from './components/welcome/Welcome'
 import RegisterUser from './components/register/RegisterUser'
 import LogIn from './components/login/LogIn'
+import RegisterOrganization from './components/register/RegisterOrganization'
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Welcome/>}/>
       <Route path='/registeruser' element={<RegisterUser/>}/>
       <Route path='/login' element={<LogIn/>}/>
+      <Route path='/registerorganization' element={<RegisterOrganization/>}/>
     </Routes>
   )
 }
