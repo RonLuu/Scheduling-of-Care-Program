a7a650ebf6d2bcc5dc34f9873a67e00a73468cb6
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-09-20 08:21:21 +1000

    Add transition to the navigation tab

--- client/src/components/NavigationTab.jsx
+++ client/src/components/NavigationTab.jsx
@@ -3,9 +3,9 @@ import { BiMenu, BiSolidDashboard, BiCalendar, BiGroup, BiTask, BiUser, BiExit }
 import { Link } from 'react-router-dom';
 import "../styles/NavigationTab.css"
 const NavigationTab = () => {
-    const [showTab, setShowTab] = useState(true)
+    const [showTab, setShowTab] = useState(false)
     return (
-        <div className={`navigationtab-panel ${showTab ? "on" : "off"}`}>
+        <div className='navigationtab-wrapper'>
             <div className='navigationtab-button-menu-wrapper'>
                 <button
                     className={`navigationtab-button-menu ${showTab ? "on" : "off"}`}
@@ -13,24 +13,27 @@ const NavigationTab = () => {
                     <BiMenu />
                 </button>
             </div>
-            {
-                showTab ? 
-                (
-                    <div className='navigationtab-button-link-wrapper'>
-                        {/* Add URL to these Link */}
-                        <Link to="/faq" className='navigationtab-link'><BiSolidDashboard className='navigationtab-icon'/>FAQ</Link>
-                        <Link to="/calendar" className='navigationtab-link'><BiCalendar className='navigationtab-icon'/>Calendar</Link>
-                        <Link to="/budgetreport" className='navigationtab-link'><BiGroup className='navigationtab-icon'/>Budget reports</Link>
-                        <Link to="/tasks" className='navigationtab-link'><BiTask className='navigationtab-icon'/>Tasks</Link>
-                        <Link to="/profile" className='navigationtab-link'><BiUser className='navigationtab-icon'/>Profile</Link>
-                        <Link to="/logout" className='navigationtab-link'><BiExit className='navigationtab-icon'/>Logout</Link>
-                    </div>
-                )
-                :
-                (
-                    <></>
-                )
-            }
+            {showTab && <div className="navigationtab-overlay" onClick={() => setShowTab(false)}></div>}
+            <div className={`navigationtab-panel ${showTab ? "on" : "off"}`}>
+                {
+                    showTab ? 
+                    (
+                        <div className='navigationtab-link-wrapper'>
+                            {/* Add URL to these Link */}
+                            <Link to="/faq" className='navigationtab-link'><BiSolidDashboard className='navigationtab-icon'/>FAQ</Link>
+                            <Link to="/calendar" className='navigationtab-link'><BiCalendar className='navigationtab-icon'/>Calendar</Link>
+                            <Link to="/budgetreport" className='navigationtab-link'><BiGroup className='navigationtab-icon'/>Budget reports</Link>
+                            <Link to="/tasks" className='navigationtab-link'><BiTask className='navigationtab-icon'/>Tasks</Link>
+                            <Link to="/profile" className='navigationtab-link'><BiUser className='navigationtab-icon'/>Profile</Link>
+                            <Link to="/logout" className='navigationtab-link'><BiExit className='navigationtab-icon'/>Logout</Link>
+                        </div>
+                    )
+                    :
+                    (
+                        <></>
+                    )
+                }
+            </div>
         </div>
     )
 }
