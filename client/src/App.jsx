4966092af4127826136d3422c46bd7f087dad8c9
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-09-25 20:50:33 +1000

    Import UserProfile into app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -50,7 +50,7 @@ const App = () => {
         path="/profile"
         element={
           <RequireAuth>
-            <ProfilePage />
+            <UserProfile />
           </RequireAuth>
         }
       />
